6b81ef7b0c47fb84e3dcfa30a09b1df72da663fc
Jan-Phil Wagner <janphil.wagner@gmail.com>
2025-11-30 22:34:21 +0100

    tidying

--- nest-backend/src/seeds/transactions.seed.ts
+++ nest-backend/src/seeds/transactions.seed.ts
@@ -1,4 +1,3 @@
-// src/seeds/transactions.seed.ts
 import { DataSource } from 'typeorm';
 import Transaction from '../transactions/entities/transaction.entity';
 import Account from '../accounts/entities/account.entity';
@@ -23,12 +22,12 @@ export async function seedTransactions(
 
     for (let i = 0; i < transactionCount; i++) {
       let processed: Date | null = null;
-      const amount = randomInt(-500, 500000); // if you store as plain number (e.g. 2 decimals)
+      const amount = randomInt(-500, 500000);
       if (randomInt(2) >= 1) processed = new Date(now.getTime());
       const tx = transactionRepo.create({
         account: { iban: account.iban } as Account,
         description: 'seeded transaction',
-        amount: amount, // or 'balanceAfter'
+        amount: amount,
         processedAt: processed,
       });
 
